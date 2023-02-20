4bb0f1db5ab7e1464b940fa6d28702f2175305a7
imsuyeon <yeonim1104@gmail.com>
2023-02-20 13:04:34 +0900

    last

--- src/component/page/Home.js
+++ src/component/page/Home.js
@@ -1,20 +1,20 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Home = () => {
     return (
         <div>
           <div className='bg'>
-            React spa
+          <span className='img-area img-area2'></span>
+           <span className='title'> MOVIES</span>
+            <span className='img-area'></span>
           </div>
           <main id='main'>
             <section id='section1'>
-                <div className='container'>
-                    <p>section1</p>
-                </div>
-                </section>
-            <section id='section2'>
-                <div className='container'>
-                <p>section2</p>
+                <div className='container section1container'>
+                    <p className='text'>If you want to add a movie, click here!</p>
+                    <p className='icon'>👇</p>
+                    <p><Link to='movies' className='movies'>Movies</Link></p>
                 </div>
                 </section>
           </main>
