18ecdb23d9d45bbf8d08adecdefb405bbe38ab3a
imsuyeon <yeonim1104@gmail.com>
2023-02-20 13:13:19 +0900

    lasst

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './component/page/Home';
 import Movies from './component/page/Movies';
 import Users from './component/page/Users';
 import Footer from './component/page/Footer';
+import {Link} from "react-router-dom";
 
 function App() {
   return (
@@ -16,6 +17,20 @@ function App() {
       <Route path='movies' element={<Movies/>}/>
       <Route path='users' element={<Users/>}/>
     </Routes>
+    <div className='bg'>
+          <span className='img-area img-area2'></span>
+           <span className='title'> MOVIES</span>
+            <span className='img-area'></span>
+          </div>
+          <main id='main'>
+            <section id='section1'>
+                <div className='container section1container'>
+                    <p className='text'>If you want to add a movie, click here!</p>
+                    <p className='icon'>👇</p>
+                    <p><Link to='movies' className='movies'>Movies</Link></p>
+                </div>
+                </section>
+          </main>
     <Footer />
    </div>
   );
